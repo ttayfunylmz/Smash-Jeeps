fe9410d2682de347fc70ce38045db4664ee16038
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2025-03-02 14:16:08 +0300

    v0.28 UI Improvements

--- Assets/_GameAssets/Scripts/UI/MainMenuUI.cs
+++ Assets/_GameAssets/Scripts/UI/MainMenuUI.cs
@@ -1,23 +1,30 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Button _hostButton;
     [SerializeField] private Button _clientButton;
     [SerializeField] private Button _lobbiesButton;
+    [SerializeField] private Button _closeButton;
     [SerializeField] private TMP_InputField _joinCodeInputField;
     [SerializeField] private GameObject _lobbiesParentGameObject;
+    [SerializeField] private RectTransform _lobbiesBackgroundTransform;
+
+    [Header("References")]
+    [SerializeField] private float _animationDuration = 1f;
+
 
     private void Awake()
     {
         _hostButton.onClick.AddListener(StartHost);
         _clientButton.onClick.AddListener(StartClient);
-        _lobbiesButton.onClick.AddListener(() =>
-        {
-            _lobbiesParentGameObject.SetActive(true);
-        });
+        _lobbiesButton.onClick.AddListener(OpenLobbies);
+        _closeButton.onClick.AddListener(CloseLobbies);
+
     }
 
     private async void StartHost()
@@ -28,5 +35,19 @@ public class MainMenuUI : MonoBehaviour
     private async void StartClient()
     {
         await ClientSingleton.Instance.ClientManager.StartClientAsync(_joinCodeInputField.text);
+    }
+
+    private void OpenLobbies()
+    {
+        _lobbiesParentGameObject.SetActive(true);
+        _lobbiesBackgroundTransform.DOAnchorPosX(-650f, _animationDuration).SetEase(Ease.OutBack);
+    }
+
+    private void CloseLobbies()
+    {
+        _lobbiesBackgroundTransform.DOAnchorPosX(900f, _animationDuration).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            _lobbiesParentGameObject.SetActive(false);
+        });
     }
 }
